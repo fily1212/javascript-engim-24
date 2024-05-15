58c60e2e55a186022267ae72cffe3be56b0c8950
fily1212 <fily1212@gmail.com>
2024-05-15 15:03:26 +0200

    lezione 2

--- lezione2/script.js
+++ lezione2/script.js
@@ -1,1 +1,20 @@
 console.log("Hello world");
+// commento a linea singola
+/*
+commento
+a più linee
+*/
+
+// assegnamento
+var a = "Hello";
+var b = "World";
+b = b.toUpperCase();
+
+var n1 = 3;
+var n2 = 2;
+
+console.log(n1+n2);
+// fate la sottrazione, -
+// moltiplicazione *
+// divisione /
+//  resto %
