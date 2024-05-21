e5ab4d60ddadf84764362abe5fabe7e59c6f01c4
fily1212 <fily1212@gmail.com>
2024-05-21 14:47:20 +0200

    lezione 3

--- lezione3/script2.js
+++ lezione3/script2.js
@@ -2,9 +2,25 @@ var numero = 0;
 document.getElementById("titolo").innerHTML = numero;
 
 function incrementa(){
-    numero++;
+    if(numero < 10){
+        numero++;
+    }
     document.getElementById("titolo").innerHTML = numero;
 }
+
+function decrementa(){
+    if(numero > 0){
+        numero--;
+    }
+    document.getElementById("titolo").innerHTML = numero;
+}
+
+/*
+if(CONDIZIONE){
+    ISTRUZIONI CHE VOGLIAMO FARE SOLO SE 
+    LA CONDIZIONE È VERA
+}
+*/
 
 
 
