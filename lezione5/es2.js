26a4c4c10d1dd26ee4177ad36f92aa53a602385a
fily1212 <fily1212@gmail.com>
2024-05-21 17:47:15 +0200

    nuovo esercizio

--- lezione5/es2.js
+++ lezione5/es2.js
@@ -6,7 +6,13 @@ function click_aggiungi(){
     // <li> </li>
     nuovo_li.innerHTML = testo_input;
     // <li> ... testo_input ... </li>
-    nuovo_li.className = "list-group-item";
+    nuovo_li.className = "list-group-item d-flex justify-content-between";
+    let bottone_cancella = document.createElement("button");
+    bottone_cancella.className = "btn btn-outline-danger btn-sm";
+    bottone_cancella.innerHTML = "-";
+    bottone_cancella.setAttributeNode(document.createAttribute("type", "button"))
+    nuovo_li.appendChild(bottone_cancella);
     document.getElementById("lista").appendChild(nuovo_li);
     
 }
+//<button type="button" class="btn btn-outline-danger">-</button>
