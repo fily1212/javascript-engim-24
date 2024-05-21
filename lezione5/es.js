aced0a5f894a87ab97e3bf6832c636b72bb4b37c
fily1212 <fily1212@gmail.com>
2024-05-21 15:46:46 +0200

    es lista

--- lezione5/es.js
+++ lezione5/es.js
@@ -8,5 +8,13 @@ console.log(giorniDellaSettimana);*/
 
 var elementi = [];
 function click_aggiungi(){
-    alert("Ciao");
+    let testo_input = document.getElementById("input-elemento").value;
+    elementi.push(testo_input);
+    let nuovo_li = document.createElement("li");
+    // <li> </li>
+    nuovo_li.innerHTML = testo_input;
+    // <li> ... testo_input ... </li>
+    nuovo_li.className = "list-group-item";
+    document.getElementById("lista").appendChild(nuovo_li);
+    
 }
