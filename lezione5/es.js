b78c48a11185679734cb3f1ddd678e83da4c21d1
fily1212 <fily1212@gmail.com>
2024-05-21 15:08:48 +0200

    esercizio lista

---
+++ lezione5/es.js
@@ -0,0 +1,7 @@
+var giorniDellaSettimana = [
+    "lun", "mar", "mer", "gio", "ven", "sab", "dom"
+];
+console.log(giorniDellaSettimana);
+console.log(giorniDellaSettimana[0]);
+giorniDellaSettimana[0] = "Lunedì";
+console.log(giorniDellaSettimana);
