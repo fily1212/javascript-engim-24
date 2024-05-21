547841703052525bfad3986b922e660ed81f701e
fily1212 <fily1212@gmail.com>
2024-05-21 15:27:13 +0200

    es lista

--- lezione5/es.js
+++ lezione5/es.js
@@ -1,7 +1,12 @@
-var giorniDellaSettimana = [
+/*var giorniDellaSettimana = [
     "lun", "mar", "mer", "gio", "ven", "sab", "dom"
 ];
 console.log(giorniDellaSettimana);
 console.log(giorniDellaSettimana[0]);
 giorniDellaSettimana[0] = "Lunedì";
-console.log(giorniDellaSettimana);
+console.log(giorniDellaSettimana);*/
+
+var elementi = [];
+function click_aggiungi(){
+    alert("Ciao");
+}
