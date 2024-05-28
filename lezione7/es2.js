5de82e911622c19cb1ad739777b96f53803b5942
fily1212 <fily1212@gmail.com>
2024-05-28 17:14:10 +0200

    lezione 7

--- lezione7/es2.js
+++ lezione7/es2.js
@@ -1,3 +1,10 @@
+let persone = [{nome:"Chiara", cognome:"Foniciello"}, 
+{nome:"Giorgia", cognome:"Sangiovanni"},
+{nome:"Valerio", cognome:"Decaro"}
+];
+
+
+
 function evidenzia(){
     /*document.getElementById("lista").children.forEach(p => {
         p.classList.add("fw-bold");
