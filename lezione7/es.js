2dd7f5879984ab795bf18631e344ea412bca1bd3
fily1212 <fily1212@gmail.com>
2024-05-28 16:09:31 +0200

    lezione 7

--- lezione7/es.js
+++ lezione7/es.js
@@ -4,12 +4,24 @@ var nomi = ["Chiara", "Giorgia", "Valerio", "Santa", "Francesco"]
     AZIONI... 
 }*/
 let i = 0;
-listaDiv = document.getElementById("lista");
-let p = document.createElement("p");
-p.innerHTML = nomi[i];
-listaDiv.appendChild(p);
+let listaDiv = document.getElementById("lista");
 
 while(i < 5){
+    let p = document.createElement("p");
+    p.innerHTML = nomi[i];
+    listaDiv.appendChild(p);
     //alert(nomi[i]);
     i++;
 }
+
+nomi.forEach(function(elem){
+    let p = document.createElement("p");
+    p.innerHTML = elem;
+    listaDiv.appendChild(p);
+});
+
+nomi.forEach((elem)=>{
+    let p = document.createElement("p");
+    p.innerHTML = elem;
+    listaDiv.appendChild(p);
+});
