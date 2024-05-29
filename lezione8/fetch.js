44f8d45463d68d27b942702263a74cbf5024665d
fily1212 <fily1212@gmail.com>
2024-05-29 15:17:51 +0200

    lezione 8

---
+++ lezione8/fetch.js
@@ -0,0 +1,20 @@
+
+function mostra(){
+    let numeroFermata = document.getElementById("numeroFermata").value;
+    fetch("http://gpa.madbob.org/query.php?stop="+numeroFermata)
+        .then(response => {
+      mostraOrario(response);
+   })
+   .catch(error => console.log("Si è verificato un errore!"));
+
+}
+
+function mostraOrario(passaggi){
+    
+    for(passaggio of passaggi){
+        
+    }
+
+}
+
+/*[{"line":"13","hour":"15:17:28","realtime":true},{"line":"13","hour":"15:23:50","realtime":false},{"line":"13","hour":"15:28:50","realtime":false},{"line":"13","hour":"15:38:50","realtime":true},{"line":"13","hour":"15:48:50","realtime":false},{"line":"13","hour":"15:58:20","realtime":true},{"line":"13","hour":"16:07:50","realtime":false},{"line":"13","hour":"16:17:50","realtime":false},{"line":"13","hour":"16:25:50","realtime":false},{"line":"13","hour":"16:32:50","realtime":false},{"line":"13","hour":"16:39:50","realtime":false},{"line":"13","hour":"16:47:50","realtime":false},{"line":"13","hour":"16:54:50","realtime":false},{"line":"13","hour":"17:01:50","realtime":false}] */
