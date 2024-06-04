efa7765514bca0c9d6cf30e45e07f97381dd9176
fily1212 <fily1212@gmail.com>
2024-06-04 15:24:48 +0200

    esercizio maglie

---
+++ lezione11/prodotti.js
@@ -0,0 +1,9 @@
+/*Creare una pagina Prodotti.html 
+che mostra in un div un elenco di card 
+per il seguente oggetto:
+
+let prodotti  = [ {“titolo”: “Maglia”, “prezzo”: 25.00, “stelle”: 4}, {“titolo”: “Pantaloni”, “prezzo”: 21.00, “stelle”: 3}, {“titolo”: “T-shirt”, “prezzo”: 15.00, “stelle”: 4.7}, {“titolo”: “Felpa”, “prezzo”: 29.00, “stelle”: 4.5}, {“titolo”: “Giacca”, “prezzo”: 55.00, “stelle”: 3.9} ] 
+
+Aggiungere un form con tre campi 
+(titolo, prezzo, stelle) e un pulsante 
+Aggiungi che aggiunge una card nuova al fondo. */
